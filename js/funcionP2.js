13c3d801d2527aed2331e76abbace6a2ba1a6df3
ToniSach <57017074+ToniSach@users.noreply.github.com>
2020-03-22 02:13:22 -0600

    Update funcionP2.js

--- js/funcionP2.js
+++ js/funcionP2.js
@@ -3,7 +3,7 @@ function funcionxd(){
     var ctx = canvas.getContext("2d");
     var imagen1;
     imagen1 = new Image();
-    imagen1.src = 'img/img01.jpg';
+    imagen1.src = 'https://scontent.fmex10-2.fna.fbcdn.net/v/t1.15752-9/90600930_519182265409945_5129776417126481920_n.jpg?_nc_cat=100&_nc_sid=b96e70&_nc_ohc=0CQPEjMdonsAX_EX1dY&_nc_ht=scontent.fmex10-2.fna&oh=340cf36dfb470b757f11ed1e804cde9b&oe=5E9D85D8';
     imagen1.onload = function (){
         ctx.drawImage(imagen1, 0, 0, canvas.width, canvas.height);
     }   
