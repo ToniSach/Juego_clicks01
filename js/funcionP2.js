ae6676a43f4c01b89321e37cdb7c3952a1193f36
ToniSach <57017074+ToniSach@users.noreply.github.com>
2020-03-22 01:05:05 -0600

    Update funcionP2.js

--- js/funcionP2.js
+++ js/funcionP2.js
@@ -1,14 +1,8 @@
-window.onload = function(){
-    var c = document.getElementById('canvas');
-    var ctx = c.getContext('2d');
-    ctx.fillStyle="rgb(250, 219, 216)";
-    ctx.font = "bold 22px Comic-Sams MS";
-    ctx.fillText("Debes completar: "+numfin+" clicks en menos de",50,50);
-    ctx.fillText("10 segundos, para ganar.",50,90);
-    var img = new Image();
-    img.src = "img/img1.JPG";  
-    img.onload = function(){
-        ctx.drawImage(img, 32.5, 100, 450, 200);
-    }
-    //document.getElementById('boton1').disabled=true;
+function insertar_ralph(){
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext("2d");
+    var imagen;
+    imagen = new Image();
+    imagen.src = 'img/img1.jpg';
+    ctx.drawImage(imagen, 32.5, 100, 450, 200);
 }
